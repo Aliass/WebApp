5ac23b3d2132a39f6314837581a1fc1396a63da2
Aliass <d.bohro@web.de>
2014-07-17 16:09:19 +0200

    New Files

---
+++ js/simple.js
@@ -0,0 +1,11 @@
+function ersterNav(){
+	 window.location.href = "index.html";
+}
+
+function zweiterNav(){
+	 window.location.href = "info.html";
+}
+
+function dritterNav(){
+	window.location.href = "kontakt.html";
+}
